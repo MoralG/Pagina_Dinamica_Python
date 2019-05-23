b83eb67b22ec8e22568b01f85cd0a2958c5b6e58
Alejandro Morales <ale95mogra@gmail.com>
2019-05-23 10:55:14 +0200

    Creando directorio static

--- app.py
+++ app.py
@@ -1,33 +1,57 @@
+# 
+# 
+# import requests
+# import os
+# 
+# import hashlib
+# mikey=os.environ["MiKey"]
+# mikeypublica=os.environ["MiKeyPublica"]
+# h = hashlib.new("md5",bytes(mikey,"utf-8"))
+# 
+# hash1=h.hexdigest()
+# 
+# URL_BASE="http://gateway.marvel.com/v1/public/"
+# 
+# PrimerNombre="Stan"
+# Apellido="Lee"
+# 
+# parametros={
+    # 'firstName':PrimerNombre,
+    # 'lastName':Apellido,
+    # 'ts':'1',
+    # 'apikey': mikeypublica,
+    # 'hash':hash1
+# }
+# 
+# r=requests.get(URL_BASE+"creators",params=parametros)
+# if r.status_code == 200:
+    # datos=r.json()
+#    
+    # for dic in datos['data']['results'][0]['series']['items']:
+        # print(dic['name']
+# 
+
+
+
+
+# ---------------------------------------
+import os
 port=os.environ["PORT"]
 
-import requests
-import os
+from flask import Flask, render_template
+app = Flask(__name__)	
 
-import hashlib
-mikey=os.environ["MiKey"]
-mikeypublica=os.environ["MiKeyPublica"]
-h = hashlib.new("md5",bytes(mikey,"utf-8"))
+@app.route('/')
+def inicio():
+    return render_template("index.html")
 
-hash1=h.hexdigest()
+@app.route('/1')
+def articulos():
+    return render_template("left-sidebar.html")
 
-URL_BASE="http://gateway.marvel.com/v1/public/"
+@app.route('/2')
+def acercade():
+    return render_template("no-sidebar.html")
 
-PrimerNombre="Stan"
-Apellido="Lee"
-
-parametros={
-    'firstName':PrimerNombre,
-    'lastName':Apellido,
-    'ts':'1',
-    'apikey': mikeypublica,
-    'hash':hash1
-}
-
-r=requests.get(URL_BASE+"creators",params=parametros)
-if r.status_code == 200:
-    datos=r.json()
-   
-    for dic in datos['data']['results'][0]['series']['items']:
-        print(dic['name']
 
 app.run('0.0.0.0',int(port), debug=True)
